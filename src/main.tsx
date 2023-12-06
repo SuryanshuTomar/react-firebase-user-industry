54e89e60294ac51966c24c15ae2770b9bfca199a
Suryanshu Tomar <suryanshutomar123@outlook.com>
2023-12-06 19:25:03 +0530

    firebase

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -6,8 +5,8 @@ import { UserProviderComp } from "./context/userContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	// <React.StrictMode>
-		<UserProviderComp>
-			<App />
-		</UserProviderComp>
+	<UserProviderComp>
+		<App />
+	</UserProviderComp>
 	// </React.StrictMode>
 );
